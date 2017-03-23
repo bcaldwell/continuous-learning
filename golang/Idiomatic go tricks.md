724f779003c7b1d475ecf1165ae4999ea873969d
Benjamin Caldwell <caldwellbenjamin8@gmail.com>
2017-03-22 23:57:16 -0400

    trying slider

--- golang/Idiomatic go tricks.md
+++ golang/Idiomatic go tricks.md
@@ -1,5 +1,7 @@
 # Idiomatic Go Tricks
 
+<h2 id="toc-header">Table of Contents <i class="fa fa-chevron-down" aria-hidden="true" id="toc-arrow"></i></h2>
+{:.no_toc}
 * Will be replaced with the ToC, excluding the "Contents" header
 {:toc}
 
--- jekyll/js/script.js
+++ jekyll/js/script.js
@@ -1,22 +1,5 @@
 
-var navbarBody = document.getElementById("navbar-body")
-
-document.getElementById("navbar-header").onclick = function(element){
-  var classList = element.target.classList
-  if (classList.contains("active")){
-    classList.remove("active")
-    Velocity(navbarBody, "slideUp", { duration: 400 });
-  } else {
-    classList.add("active")
-    Velocity(navbarBody, "slideDown", { duration: 400 });
-  }
-}
-
-window.onresize = function(event) {
-
-};
-
-var tocBody = document.getElementById("toc-body")
+var tocBody = document.getElementById("markdown-toc")
 var tocArrow = document.getElementById("toc-arrow")
 if (tocBody){
   document.getElementById("toc-header").onclick = function(element){
@@ -33,14 +16,3 @@ if (tocBody){
   }
   tocBodyStyle = tocBody.style;
 }
-
-navbarBodyStyle = navbarBody.style || null;
-
-window.onresize = function(event) {
-    if (navbarBodyStyle && window.innerWidth > 768){
-      navbarBodyStyle.display = null;
-    }
-    if (navbarBodyStyle && tocBodyStyle.display == "none" && window.innerWidth > 768){
-      tocBodyStyle.display = null;
-    }
-};
