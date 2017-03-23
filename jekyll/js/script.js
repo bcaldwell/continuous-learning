4d007223092b8f0f1c80d1f3925cf991c0a51ba3
Benjamin Caldwell <caldwellbenjamin8@gmail.com>
2017-03-23 00:43:13 -0400

    ummm arrows...

--- jekyll/js/script.js
+++ jekyll/js/script.js
@@ -9,11 +9,11 @@ if (tocBody){
     if (classList.contains("active")){
       classList.remove("active")
       Velocity(tocBody, "slideUp", { duration: 400 });
-      Velocity(tocArrow, {rotateZ: "0deg"} , { duration: 400 });
+      Velocity(tocArrow, {rotateZ: "180deg"} , { duration: 400 });
     } else {
       classList.add("active")
       Velocity(tocBody, "slideDown", { duration: 400 });
-      Velocity(tocArrow, {rotateZ: "180deg"} , { duration: 400 });
+      Velocity(tocArrow, {rotateZ: "0deg"} , { duration: 400 });
     }
   }
 }
